2ddebc883092307750952d212115c59f5aabb2fc
NguyenvanLuc06081994 <nvluc0608@gmail.com>
2020-08-20 10:03:54 +0700

    update

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -26,19 +26,10 @@ class AuthServiceProvider extends ServiceProvider
         $this->registerPolicies();
 
 
-        Gate::define('crud',function ($user){
-            foreach ($user->roles as $role){
-                if ($role->name === 'admin'){
-                    return  true;
-                }
-            }
-           return false;
-        });
-
-        Gate::define('update',function ($user){
-            foreach ($user->roles as $role){
-                if ($role->name === 'edit'){
-                    return  true;
+        Gate::define('crud', function ($user) {
+            foreach ($user->roles as $role) {
+                if ($role->name === 'admin') {
+                    return true;
                 }
             }
             return false;
